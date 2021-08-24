53a9da26142b7b2ca11f8c13f075370517cc2431
Konstantin Dunaev <shop.overled@gmail.com>
2021-08-24 23:17:33 +0300

    Add all test for Task 1

--- src/test/java/ru/netology/DeliveryOrderPageTest.java
+++ src/test/java/ru/netology/DeliveryOrderPageTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,8 +10,8 @@ import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -21,9 +22,33 @@ public class DeliveryOrderPageTest {
     }
 
     @Test
+    void shouldSendValidData() {
+        $("[data-test-id=city] [class='input__control']").setValue("Волгоград");
+        $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
+        $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
+        $("[class=checkbox__box]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=notification] [class=notification__content]")
+                .shouldHave(exactText("Встреча успешно забронирована на " +
+                        element("[data-test-id=date] input")
+                                .getAttribute("value")), Duration.ofSeconds(11));
+    }
+
+    @Test
     void shouldNotValidateEmptyCity(){
         $("[data-test-id=city] [class='input__control']").setValue(null);
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
+        $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
+        $("[class=checkbox__box]").click();
+        $(byText("Забронировать")).click();
+        $(byText("Поле обязательно для заполнения")).shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void shouldNotValidateEmptyName(){
+        $("[data-test-id=city] [class='input__control']").setValue("Москва");
+        $("[data-test-id=name] [class='input__control']").setValue(null);
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
         $(byText("Забронировать")).click();
@@ -61,4 +86,42 @@ public class DeliveryOrderPageTest {
 
     }
 
+
+
+    @Test
+    void shouldNotValidateNonRussianName(){
+        $("[data-test-id=city] [class='input__control']").setValue("Москва");
+        $("[data-test-id=name] [class='input__control']").setValue("Boris Yeltsin");
+        $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
+        $("[class=checkbox__box]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=name]").$(byClassName("input__sub"))
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. " +
+                        "Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldNotValidateWrongCity(){
+        $("[data-test-id=city] [class='input__control']").setValue("Караганда");
+        $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
+        $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
+        $("[class=checkbox__box]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=city]").$(byClassName("input__sub"))
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
+
+    }
+
+    @Test
+    void shouldNotValidateWrongDate(){
+        $("[data-test-id=city] [class='input__control']").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE + "0");
+        $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
+        $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
+        $("[class=checkbox__box]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=date]").$(byClassName("input__sub"))
+                .shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
 }
