b5ffb20faab5875860d7a6c3478b82c080a736d9
Konstantin Dunaev <shop.overled@gmail.com>
2021-08-26 23:34:20 +0300

    added method to get the date, added method to repeat keyboard keys, for Tasks 2 test added an input to the popup of any number of days

--- src/test/java/ru/netology/DeliveryOrderPageTest.java
+++ src/test/java/ru/netology/DeliveryOrderPageTest.java
@@ -7,42 +7,58 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static java.util.Arrays.fill;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryOrderPageTest {
+
+    public String meetDate(int days){
+       return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public CharSequence repeatKey(Keys key, int days) {
+        String[] chars = new String[days-3];
+        fill(chars, key.toString());
+        return Arrays.toString(chars);
+    }
+
     @BeforeEach
     void setUp(){
         open("http://localhost:9999");
+
     }
 
     @Test
     void shouldSendValidData() {
         $("[data-test-id=city] [class='input__control']").setValue("Волгоград");
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(7));
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=notification] [class=notification__content]")
                 .shouldHave(exactText("Встреча успешно забронирована на " +
-                        element("[data-test-id=date] input")
-                                .getAttribute("value")), Duration.ofSeconds(11));
+                        meetDate(7)), Duration.ofSeconds(11));
     }
 
     @Test
     void shouldDefaultTimeBeRight() {
-        assertEquals(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
+        assertEquals(meetDate(3),
                 element("[data-test-id=date] input")
                 .getAttribute("value"));
     }
 
     @Test
     void shouldNotValidateEmptyCity(){
-        $("[data-test-id=city] [class='input__control']").setValue(null);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(3));
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
@@ -54,7 +70,8 @@ public class DeliveryOrderPageTest {
     @Test
     void shouldNotValidateEmptyName(){
         $("[data-test-id=city] [class='input__control']").setValue("Москва");
-        $("[data-test-id=name] [class='input__control']").setValue(null);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(7));
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
         $(byText("Забронировать")).click();
@@ -76,6 +93,8 @@ public class DeliveryOrderPageTest {
     @Test
     void shouldNotValidateEmptyPhone(){
         $("[data-test-id=city] [class='input__control']").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(3));
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
         $("[class=checkbox__box]").click();
         $(byText("Забронировать")).click();
@@ -85,6 +104,8 @@ public class DeliveryOrderPageTest {
     @Test
     void shouldNotValidateEmptyCheckbox(){
         $("[data-test-id=city] [class='input__control']").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(3));
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $(byText("Забронировать")).click();
@@ -97,6 +118,8 @@ public class DeliveryOrderPageTest {
     @Test
     void shouldNotValidateNonRussianName(){
         $("[data-test-id=city] [class='input__control']").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(3));
         $("[data-test-id=name] [class='input__control']").setValue("Boris Yeltsin");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
@@ -110,6 +133,8 @@ public class DeliveryOrderPageTest {
     @Test
     void shouldNotValidateWrongCity(){
         $("[data-test-id=city] [class='input__control']").setValue("Караганда");
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"a",Keys.DELETE);
+        $("[data-test-id=date] input").setValue(meetDate(3));
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
@@ -133,18 +158,17 @@ public class DeliveryOrderPageTest {
 
     @Test
     void shouldSendValidDataByUsingPopup(){
+        int days = 7;
         $("[data-test-id=city] [class='input__control']").setValue("Ек");
         $(byClassName("menu")).$(byText("Екатеринбург")).click();
         $("[data-test-id=date]").click();
-        $(byClassName("calendar")).sendKeys(Keys.RIGHT,Keys.RIGHT,Keys.RIGHT,Keys.RIGHT,Keys.ENTER);
+        $(byClassName("calendar")).sendKeys(repeatKey(Keys.RIGHT,days),Keys.ENTER);
         $("[data-test-id=name] [class='input__control']").setValue("Борис Ельцин");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[class=checkbox__box]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=notification] [class=notification__content]")
                 .shouldHave(exactText("Встреча успешно забронирована на " +
-                        element("[data-test-id=date] input")
-                                .getAttribute("value")), Duration.ofSeconds(11));
-
+                        meetDate(days)), Duration.ofSeconds(11));
     }
 }
